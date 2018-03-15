7b016940536b00343f1d6c872766d3ed50609bed
Andreasnordboe <andreasnordboe@gmail.com>
2018-03-15 22:17:05 +0100

    Push
    Major code and basic features in place, ready for world building and setdressing.
    Updates to menu and UI.
    Bug fixes and improvements.
    
    Known bug:
    Button focus stops working after closing controls panel on some menus

---
+++ TechNoia/Intermediate/Build/Win64/TechNoia/Development/UELinkerFixups/PCH.UELinkerFixups.cpp
@@ -0,0 +1,1 @@
+#include "D:/Dropbox/NUC_IMG2_2017-2018/Ludic Studies/Games/Technoia_09/TechNoia/TechNoia/Intermediate/Build/Win64/TechNoia/Development/UELinkerFixups/PCH.UELinkerFixups.h"
---
+++ TechNoia/Intermediate/Build/Win64/TechNoia/Development/UELinkerFixups/PCH.UELinkerFixups.h
@@ -0,0 +1,2 @@
+// PCH for D:\Dropbox\NUC_IMG2_2017-2018\Ludic Studies\Games\Technoia_09\TechNoia\TechNoia\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h
+#include "D:\Dropbox\NUC_IMG2_2017-2018\Ludic Studies\Games\Technoia_09\TechNoia\TechNoia\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
